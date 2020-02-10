5d1e5b05be44a75ebce3c64b3c40e8448fe29211
frostickflakes <frostilm@dukes.jmu.edu>
2020-02-10 11:38:43 -0500

    first fizzbuzz file

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,10 @@
-
+number_given = input('What is the given number?: ')
+def fizz_buzz(input):
+        if (input % 3 == 0) and (input % 5 == 0):
+            return "FizzBuzz"
+        if input % 3 == 0:
+            return "Fizz"
+        if input % 5 == 0:
+            return "Buzz"
+        return input
+print(fizz_buzz(int(number_given)))
